071563cf6fd9902688968da5afad7eea8b45894f
vaibhav <vaibh>
2023-06-15 16:43:14 +0530

    lates

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,11 @@ app.get('/home', (req, res) => {
 
 const port = process.env.PORT
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')))
+  // '../../', 'public', 'uploads'
+  app.use(express.static(path.join(__dirname, '../', 'build')))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html'))
   })
 } else {
   app.get('*', (req, res) => {
