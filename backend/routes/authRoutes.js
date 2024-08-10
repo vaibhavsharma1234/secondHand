5d164d05a72e8df371312ea1327c2a6fcfb2ca98
vaibhav sharma <sharmavishu052@gmail.com>
2024-08-10 23:39:28 +0530

    improved login and register a little

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = require('express').Router()
 const verifyOTP = require('../middlewares/verifyOtp')
-const { signup, login } = require('../controllers/authController')
+const { signup, login,checkEmailExist } = require('../controllers/authController')
 router.post('/signup',verifyOTP ,signup)
 // router.post('/signup',signup)
 router.post('/login', login)
+router.post("/checkEmail",checkEmailExist)
 module.exports = router
